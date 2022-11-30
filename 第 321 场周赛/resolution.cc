494593c53b9adb34d8a6c7f480cfa5027fb033a3
Chen Songsong <41141531+hallucination11@users.noreply.github.com>
2022-11-30 17:27:12 +0800

    4

--- 第 321 场周赛/resolution.cc
+++ 第 321 场周赛/resolution.cc
@@ -85,7 +85,49 @@ public:
  */
 class Solution {
 public:
+
+    ListNode* dfs(ListNode* u) {
+        if (u->next == nullptr) return u;
+        u->next = dfs(u->next);
+        auto nxt = u->next;
+        if (nxt->val > u->val) return nxt;
+        return u;
+    }
+
     ListNode* removeNodes(ListNode* head) {
-        
+            ListNode *nd = new ListNode(1e9, head);
+            return dfs(nd)->next;
+    }
+};
+
+
+# 4.统计中位数为 K 的子数组
+
+给你一个长度为 n 的数组 nums ，该数组由从 1 到 n 的 不同 整数组成。另给你一个正整数 k 。
+
+统计并返回 num 中的 中位数 等于 k 的非空子数组的数目。
+
+注意：
+
+数组的中位数是按 递增 顺序排列后位于 中间 的那个元素，如果数组长度为偶数，则中位数是位于中间靠 左 的那个元素。
+例如，[2,3,1,4] 的中位数是 2 ，[8,4,3,5,1] 的中位数是 4 。
+子数组是数组中的一个连续部分。
+
+示例 1：
+
+输入：nums = [3,2,1,4,5], k = 4
+输出：3
+解释：中位数等于 4 的子数组有：[4]、[4,5] 和 [1,4,5] 。
+示例 2：
+
+输入：nums = [2,3,1], k = 3
+输出：1
+解释：[3] 是唯一一个中位数等于 3 的子数组。
+
+
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums, int k) {
+                
     }
 };
