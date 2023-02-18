a8cdfc9ebbdb59ce2e89d0b2a39786fefff130dc
Chen Songsong <17862708589@163.com>
2023-02-18 09:59:50 +0800

    322_2_2

--- 第332场周赛/solution.cc
+++ 第332场周赛/solution.cc
@@ -60,6 +60,14 @@ lower <= nums[i] + nums[j] <= upper
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        
+        long long res = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 0 ; i < nums.size() ; i++) {
+            
+            int r = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]);
+            int l = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]);
+            res += r - l;
+        }
+        return res;
     }
 };
