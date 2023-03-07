3dcb6bdd15ffeb8d20d68eb6e286f80a8fa6ccf1
Chen Songsong <41141531+hallucination11@users.noreply.github.com>
2023-03-07 10:29:31 +0800

    335_2

--- 第335场周赛/solution.cc
+++ 第335场周赛/solution.cc
@@ -29,3 +29,58 @@ public:
         return result;
     }
 };
+
+#2.二叉树中的第 K 大层和
+
+给你一棵二叉树的根节点 root 和一个正整数 k 。
+
+树中的 层和 是指 同一层 上节点值的总和。
+
+返回树中第 k 大的层和（不一定不同）。如果树少于 k 层，则返回 -1 。
+
+注意，如果两个节点与根节点的距离相同，则认为它们在同一层。
+
+输入：root = [5,8,9,2,1,3,7,4,6], k = 2
+输出：13
+解释：树中每一层的层和分别是：
+- Level 1: 5
+- Level 2: 8 + 9 = 17
+- Level 3: 2 + 1 + 3 + 7 = 13
+- Level 4: 4 + 6 = 10
+第 2 大的层和等于 13 。
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* u, int l, map<int, long long>&mp) {
+        mp[l] += u->val;
+        if (u->left != nullptr) {
+            dfs(u->left, l+1, mp);
+        }
+        if (u->right != nullptr) {
+            dfs(u->right, l+1, mp);
+        }
+    }
+    
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        map<int, long long> mp;
+        dfs(root, 1, mp);
+        if (mp.size() < k) return -1;
+        vector<long long> vt;
+        for (auto it: mp) {
+            vt.push_back(it.second);
+        }
+        sort(vt.begin(), vt.end(), greater<>());
+        return vt[k-1];
+    }
+};
