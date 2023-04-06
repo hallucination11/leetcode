a2c11a7a88aac6a6a0d98d370f3c112b6fd721e2
Chen Songsong <41141531+hallucination11@users.noreply.github.com>
2023-04-06 09:08:32 +0800

    339_2

--- 第339场周赛/solution.cc
+++ 第339场周赛/solution.cc
@@ -31,3 +31,43 @@ public:
         return res * 2;
     }
 };
+
+2.转换二维数组
+
+给你一个整数数组 nums 。请你创建一个满足以下条件的二维数组：
+
+二维数组应该 只 包含数组 nums 中的元素。
+二维数组中的每一行都包含 不同 的整数。
+二维数组的行数应尽可能 少 。
+返回结果数组。如果存在多种答案，则返回其中任何一种。
+
+请注意，二维数组的每一行上可以存在不同数量的元素。
+
+示例 1：
+
+输入：nums = [1,3,4,1,2,3,1]
+输出：[[1,3,4,2],[1,3],[1]]
+解释：根据题目要求可以创建包含以下几行元素的二维数组：
+- 1,3,4,2
+- 1,3
+- 1
+nums 中的所有元素都有用到，并且每一行都由不同的整数组成，所以这是一个符合题目要求的答案。
+可以证明无法创建少于三行且符合题目要求的二维数组。
+
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<vector<int>> ret;
+        unordered_map<int, int> mp;
+        sort(nums.begin(), nums.end());
+        for (auto x : nums) {
+            mp[x] ++;
+            int v = mp[x] - 1;
+            if (v == ret.size()) ret.push_back(vector<int>());
+            ret[v].push_back(x);
+        }
+        return ret
+    }
+}
+
+
