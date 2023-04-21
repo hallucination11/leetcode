28d293b3ea8d513ad7195eaf13cf8d182de905a4
Chen Songsong <17862708589@163.com>
2023-04-21 10:58:17 +0800

    del elem

--- code_carl/二分查找.cc
+++ code_carl/二分查找.cc
@@ -140,6 +140,55 @@ public:
 
 
 
+--移除元素
+
+给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并原地修改输入数组。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+示例 1: 给定 nums = [3,2,2,3], val = 3, 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。 你不需要考虑数组中超出新长度后面的元素。
+
+示例 2: 给定 nums = [0,1,2,2,3,0,4,2], val = 2, 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+
+
+暴力解法
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+    	int size = nums.size();
+    	for (int i = 0 ; i < nums.size() ; i ++) {
+    		if (nums[i] == val) {
+    			for (int j = i ; j < nums.size() - 1 ; j ++) {
+    				nums[j] = nums[j + 1]
+    			}
+    		}
+    		i --;
+    		size ++;
+    	}
+    	return size;
+    }
+};
+
+
+--双指针
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+    	int slowIndex = 0;
+    	for (int fastIndex = 0 ; fastIndex < nums.size() ; fastIndex ++) {
+    		if (nums[fastIndex] != val) {
+    			nums[slowIndex++] = nums[fastIndex];
+    		}
+    		
+    	}
+    	return size;
+    }
+};
+注意这些实现方法并没有改变元素的相对位置
+
+
 
 
 
