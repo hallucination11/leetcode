71305b948db4487a5178da9ead2cd227b986f6a3
Chen Songsong <17862708589@163.com>
2023-04-22 17:49:10 +0800

    square

--- code_carl/二分查找.cc
+++ code_carl/二分查找.cc
@@ -189,6 +189,35 @@ public:
 注意这些实现方法并没有改变元素的相对位置
 
 
+4.有序数组的平方
+
+给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+
+示例 1： 输入：nums = [-4,-1,0,3,10] 输出：[0,1,9,16,100] 解释：平方后，数组变为 [16,1,0,9,100]，排序后，数组变为 [0,1,9,16,100]
+
+示例 2： 输入：nums = [-7,-3,2,3,11] 输出：[4,9,9,49,121]
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+    	int k = A.size() - 1;
+    	vector<int> res(A.size(), 0);
+    	for (int i = 0, j = A.size() - 1 ; i <= j ; ) {
+    		if (A[i] * A[i] >= A[j] * A[j]) {
+    			res[k--] = A[i] * A[i];
+    			i ++;
+    		} else {
+    			res[k--] = A[j] * A[j];
+    			j --
+    		}
+    	}
+    	return res;
+    }
+}；
+
+
+
+
 
 
 
