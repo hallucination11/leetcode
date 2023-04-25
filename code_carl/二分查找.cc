a85aee62c94498042e6eaa1ed476f11f483ce54a
Chen Songsong <41141531+hallucination11@users.noreply.github.com>
2023-04-25 08:58:45 +0800

    subarray

--- code_carl/二分查找.cc
+++ code_carl/二分查找.cc
@@ -216,9 +216,60 @@ public:
 }；
 
 
+5.长度最小的子数组(滑动窗口)
+
+给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
+
+示例：
+
+输入：s = 7, nums = [2,3,1,2,4,3] 输出：2 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+
+提示：
+
+1 <= target <= 10^9
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^5
 
 
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+    	int result = INT32_MAX;
+    	int sum = 0;
+    	for (int i = 0 ; i < nums.size() - 1; i ++) {
+    		sum = 0;
+    		for (int j = i ; j < nums.size() - 1 ; j ++) {
+    			sum += nums[j];
+    			if (sum >= s) {
+    				subLength = j - i + 1;
+    				result = result < subLength ? result : subLength;
+    				break;
+    			}
+    		}
+    	}
+    	return result == INT32_MAX ? 0 : result;
+    }
+};
 
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+    	int result = INT32_MAX;
+    	int sum = 0;
+    	int subLength = 0;
+    	int i = 0;
+    	for (int j = 0 ; j < nums.size() - 1; j ++) {
+    		sum += nums[j];
+    		while (sum >= s) {
+    			subLength = j - i + 1;
+    			result = result < subLength ? result : subLength;
+    			sum -= nums[i++]; 
+    		}
+
+    	}
+    	return result == INT32_MAX ? 0 : result;
+    }
+};
 
 
 
