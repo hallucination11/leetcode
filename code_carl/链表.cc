84c11de98726610dfce709850dd040d0aa68e9c5
Chen Songsong <17862708589@163.com>
2023-04-29 10:48:39 +0800

    reverse list

--- code_carl/链表.cc
+++ code_carl/链表.cc
@@ -71,4 +71,36 @@ deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 inde
 
 示例: 输入: 1->2->3->4->5->NULL 输出: 5->4->3->2->1->NULL
 
+--双指针
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* pre = NULL;
+        while (cur != NULL) {
+            temp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+};
 
+
+--递归法
+class Solution {
+public:
+    ListNode* reverse(ListNode* pre, ListNode* cur) {
+        if (cur == NULL) return pre;
+        ListNode* temp = cur->next;
+        cur->next = pre;
+        return reverse(cur, temp);
+
+    }
+
+
+    ListNode* reverseList(ListNode* head) {
+        return reverse(NULL, head);
+    }
+};
