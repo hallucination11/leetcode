e09ac5f85ad661e2b66ddfd16af3a617daf72fbd
Chen Songsong <41141531+hallucination11@users.noreply.github.com>
2023-05-01 10:14:19 +0800

    list Intersect

--- code_carl/链表.cc
+++ code_carl/链表.cc
@@ -162,3 +162,25 @@ public:
         return dummyHead->next;
     }
 };
+
+7.链表相交
+
+给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (headA == nullptr || headB == nullptr) {
+            return nullptr;
+        }
+        ListNode *pA = headA, *pB = headB;
+        while (pA != pB) {
+            pA = pA == nullptr ? headB : pA->next;
+            pB = pB == nullptr ? headA : pB->next;
+        }
+        return pA;
+    }
+};
+
+
+
