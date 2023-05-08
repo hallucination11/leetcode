fff791d173f072a58016753600d597a6c8f20c43
Chen Songsong <41141531+hallucination11@users.noreply.github.com>
2023-05-08 08:36:09 +0800

    hash 9

--- code_carl/hash_table.cc
+++ code_carl/hash_table.cc
@@ -239,17 +239,6 @@ public:
     }
 };
 
-9.四数之和
-
-题意：给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？
-
-找出所有满足条件且不重复的四元组。
-
-注意：
-
-答案中不可以包含重复的四元组。
-
-示例： 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。 满足要求的四元组集合为： [ [-1, 0, 0, 1], [-2, -1, 1, 2], [-2, 0, 0, 2] ]
 
 
 5.给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -277,4 +266,74 @@ public:
         return {};
     }
 };
+
+
+9.四数之和
+
+题意：给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？
+
+找出所有满足条件且不重复的四元组。
+
+注意：
+
+答案中不可以包含重复的四元组。
+
+示例： 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。 
+
+满足要求的四元组集合为： [[-1, 0, 0, 1], [-2, -1, 1, 2], [-2, 0, 0, 2]]
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+        vector<vector<int>> result;
+
+        sort(nums.begin(), nums.end());
+
+        for (int k = 0 ; k < nums.size() ; k ++) {
+            if (nums[k] > target && k >= 0) {
+                break;
+            }
+
+            if (k > 0 && nums[k] == nums[k - 1]) {
+                continue;
+            }
+
+            for (int i = k + 1 ; i < nums.size() ; i ++) {
+                if (nums[k] + nums[i] > target && nums[k] + nums[i] > 0) {
+                    break;
+                }
+
+                if (i > k + 1 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+
+                int left = i + 1, right = nums.size() - 1;
+
+                while (right > left) {
+                    if ((long) nums[k] + nums[i] + nums[left] + nums[right] > target) {
+                        right--;
+                    } else if ((long) nums[k] + nums[i] + nums[left] + nums[right]  < target) {
+                        left++;
+                    } else {
+                        result.push_back(vector<int>{nums[k], nums[i], nums[left], nums[right]});
+                        // 对nums[left]和nums[right]去重
+                        while (right > left && nums[right] == nums[right - 1]) right--;
+                        while (right > left && nums[left] == nums[left + 1]) left++;
+
+                        // 找到答案时，双指针同时收缩
+                        right--;
+                        left++;
+                    }
+                }
+            }
+        } 
+
+        return result;
+
+    }
+};
+
+
+
 
