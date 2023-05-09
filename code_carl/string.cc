e0d6a461949dfd63fc556f49a4f0314cf4511d17
Chen Songsong <41141531+hallucination11@users.noreply.github.com>
2023-05-09 17:05:09 +0800

    string_5

--- code_carl/string.cc
+++ code_carl/string.cc
@@ -85,5 +85,59 @@ public:
 };
 
 
+4.翻转字符串里的单词
+
+给定一个字符串，逐个翻转字符串中的每个单词。
+
+示例 1：
+输入: "the sky is blue"
+输出: "blue is sky the"
+
+示例 2：
+输入: "  hello world!  "
+输出: "world! hello"
+解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+
+示例 3：
+输入: "a good   example"
+输出: "example good a"
+解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个
+
+class Solution {
+public:
+    string reverseWords(string s) {
+
+    }
+};
+
+
+5.左旋转字符串
+
+字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。
+
+比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+
+示例 1：
+输入: s = "abcdefg", k = 2
+输出: "cdefgab"
+
+示例 2：
+输入: s = "lrloseumgh", k = 6
+输出: "umghlrlose"
+
+限制：
+1 <= k < s.length <= 10000
+
+class Solution {
+public:
+    string reverseLeftWords(string s, int n) {
+        reverse(s.begin(), n);
+        reverse(s.begin() + n, s.end());
+        reverse(s.begin, s.end());
+
+        return s;
+    }
+};
+
 
 
