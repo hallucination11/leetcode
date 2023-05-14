49e787d65f0a9951fc2f01fa2bf41b220d10e275
Chen Songsong <17862708589@163.com>
2023-05-14 11:21:57 +0800

    stach_6

--- code_carl/栈与队列.cc
+++ code_carl/栈与队列.cc
@@ -238,3 +238,51 @@ public:
     	
     }
 };
+
+
+7.滑动窗口最大值
+
+给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+
+返回滑动窗口中的最大值。
+
+进阶：
+
+你能在线性时间复杂度内解决此题吗？
+
+class Solution {
+private:
+    class MyQueue {
+    	deque<int> que;
+
+    	void pop(int value) {
+    		if (!que.empty() && que.front() == value) {
+    			que.pop_front();
+    		}
+    	}
+
+    	void push(int value) {
+    		while (!que.empty() && value > que.back()) {
+                que.pop_back();
+            }
+            que.push_back(value);
+    	}
+    }
+
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    	deque<int> que;
+    	vector<int> result;
+    	for (int i = 0 ; i < k ; i ++) {
+    		que.push(nums[i]);
+    	}
+    	result.push_back(que.front());
+    	for (int i = k ; i < nums.size() ; i ++) {
+    		que.pop(nums[i - k]); 
+            que.push(nums[i]); 
+            result.push_back(que.front());
+    	}
+    	return result;
+    }	
+
+};
